c2c747cc4693b0a9e41d2b89e52b409a30dcde0a
typeproto187 <55107585+typeproto187@users.noreply.github.com>
2019-12-21 21:19:26 -0600

    Update 00002-custom-boot-animation.sh

--- 00002-custom-boot-animation.sh
+++ 00002-custom-boot-animation.sh
@@ -4,4 +4,4 @@ BOOT_ANIMATION="https://github.com/RattlesnakeOS/example_patch_shellscript/raw/m
 mkdir -p $BUILD_DIR/branding
 echo "Downloading boot animation $BOOT_ANIMATION"
 wget -O $BUILD_DIR/branding/bootanimation.zip $BOOT_ANIMATION
-sed -i "\$aPRODUCT_COPY_FILES += branding/bootanimation.zip:system/media/bootanimation.zip" ${BUILD_DIR}/build/make/target/product/core.mk
+sed -i "\$aPRODUCT_COPY_FILES += branding/bootanimation.zip:system/media/bootanimation.zip" ${BUILD_DIR}/build/make/target/product/handheld_system.mk
